e7f059ced04f5e86a127632bc705a31b6ec318de
Chia Xiong <cyx.cyxiong@gmail.com>
2023-02-05 16:41:10 -0600

    adding a bunch of config files

--- src/components/NavBar/Styles/NavBarStyle.js
+++ src/components/NavBar/Styles/NavBarStyle.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.div`
+  // since the navbar is a layout style; the body is included here
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
   display: flex;
   justify-content: center;
   gap: 100px;
